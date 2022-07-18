ff9760492b54625327305e7bdce6c330d24334bf
Your Name <kishore9657@gmail.com>
2022-07-18 21:31:42 +0600

    Added log in register page

--- src/App.vue
+++ src/App.vue
@@ -1,83 +1,11 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import {RouterLink, RouterView} from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    My app new Data Added from jetbrains
-  </div>
-  <p>new Changes for test purpose</p>
- <i class="fa fa-address-book text-success"></i>
-  <a class="btn btn-primary mr-2" href="/">Home</a>
-  <a class="btn btn-primary mr-2" href="/about">About</a>
-  <a class="btn btn-primary mr-2" href="/about">Login</a>
-  <a class="btn btn-primary mr-2" href="/about">Register</a>
-<!--  <RouterView />-->
+  <RouterView/>
 </template>
 
 <style scoped>
-
-
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -10,13 +10,24 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/login',
+      name: 'login',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
+      component: () => import('../views/LoginView.vue')
+    },
+    {
+      path: '/about-us',
+      name: 'about-us',
       component: () => import('../views/AboutView.vue')
-    }
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('../views/RegisterView.vue')
+    },
+
   ]
 })
 
